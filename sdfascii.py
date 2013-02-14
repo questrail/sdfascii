fc9e44a156783e14c128bde3d6f9e4f51ff2490f
Matthew Rankin <matthew@questrail.com>
2013-02-14 11:28:12 -0600

    Partially implemented SDF meas_hdr decoder

--- sdfascii.py
+++ sdfascii.py
@@ -4,6 +4,9 @@
 sdfascii.py
 
 Read SDF and ASCII files created by HP/Agilent Dynamic Signal Analyzers.
+
+**WARNING:** Currently, only supporting SDF version 2 as produced by an
+Agilent 35670A DSA.
 '''
 
 # Try to future proof code so that it's Python 3.x ready
@@ -13,11 +16,16 @@ from __future__ import division
 from __future__ import absolute_import
 
 # Standard module imports
+import datetime
+import string
 import struct
 import sys
 
 # Data analysis related imports
 import numpy as np
+
+def _strip_nonprintable(input_string):
+    return input_string.split('\x00',1)[0]
 
 def _read_hdr(input_hdr_filename):
     pass
@@ -34,12 +42,103 @@ def _read_z(input_z_filename):
 def read_ascii(input_ascii_base_filename):
     pass
 
-def _process_sdf_file_hdr(record_type, record_size, binary_data):
+def _decode_sdf_file_hdr(record_size, binary_data):
     '''
-    record_size is the total size of the binary_data including the
-    record_type and record_size, which are not in binary_data
+    * record_size is the total size of the binary_data including the
+    record_type and record_size
+    * file_hdr_binary contains all the binary data for the file header
+    including the record_type and record_size.
+    * The HP documentation lists the binary indices starting a 1, whereas
+    Python uses 0 based arrays/indices
     '''
-    return {}
+    file_hdr = {}
+    file_hdr['record_size'] = record_size
+    file_hdr['sdf_revision'], = struct.unpack('>h', binary_data[6:8])
+    application_code, = struct.unpack('>h', binary_data[8:10])
+    application_decoder = { -1: 'HP VISTA', -2: 'HP SINE', -3: 'HP 35660A', 
+            -4: 'HP 3562A, HP 3563A', -5: 'HP 3588A', -6: 'HP 3589A', -99: 'Unknown',
+            1: 'HP 3566A, HP 3567A', 2: 'HP 35665A', 3: 'HP 3560A', 
+            4: 'HP 89410A, HP 89440A', 7: 'HP 35635R', 8: 'HP 35654A-S1A',
+            9: 'HP 3569A', 10: 'HP 35670A', 11: 'HP 3587S'}
+    file_hdr['application'] = application_decoder[application_code]
+    msr_year, msr_month_day, msr_hour_min = struct.unpack('>hhh', binary_data[10:16])
+    msr_month, msr_day = divmod(msr_month_day, 100)
+    msr_hour, msr_sec = divmod(msr_hour_min, 100)
+    file_hdr['measurement_start_datetime'] = datetime.datetime(msr_year,
+            msr_month, msr_day, msr_hour, msr_sec)
+    file_hdr['application_version'] = _strip_nonprintable(
+            struct.unpack('>8s', binary_data[16:24])[0])
+    (file_hdr['num_data_hdr_records'], file_hdr['num_vector_hdr_records'],
+            file_hdr['num_channel_hdr_records'], file_hdr['num_unique_records'],
+            file_hdr['num_scan_struct_records'], file_hdr['num_xdata_records']) = \
+                    struct.unpack('>6h', binary_data[24:36])
+    (file_hdr['offset_data_hdr_record'], file_hdr['offset_vector_record'],
+            file_hdr['offset_channel_record'], file_hdr['offset_unique_record'],
+            file_hdr['offset_scan_struct_record'], file_hdr['offset_xdata_record'],
+            file_hdr['offset_ydata_record']) = \
+            struct.unpack('>7l', binary_data[36:64])
+
+    if file_hdr['sdf_revision'] == 3:
+        # Continue reading bytes 65-80 if this is SDF ver. 3
+        # FIXME: Need to add the code for SDF ver. 3 in the future.
+        pass
+
+    return file_hdr
+
+def _decode_sdf_meas_hdr(record_size, sdf_revision, binary_data):
+    '''
+    Decode the measurement header binary data
+    '''
+    meas_hdr = {}
+    meas_hdr['record_size'] = record_size
+    (meas_hdr['offset_unique_record'],) = \
+            struct.unpack('>1l', binary_data[6:10])
+    meas_hdr['block_size'], = struct.unpack('>l', binary_data[18:22])
+    meas_hdr['zoom_mode_on'], = struct.unpack('>h', binary_data[22:24])
+    meas_hdr['zoom_mode_on'] = bool(meas_hdr['zoom_mode_on'])
+    coded_average_type, = struct.unpack('>h', binary_data[28:30])
+    average_type_decoder = {0: 'None', 1: 'RMS', 2: 'RMS Exponential',
+            3: 'Vector', 4: 'Vector Exponential', 5: 'Continuous Peak Hold',
+            6: 'Peak'}
+    meas_hdr['average_type'] = average_type_decoder[coded_average_type]
+    meas_hdr['average_num'], = struct.unpack('>l', binary_data[30:34])
+    meas_hdr['pct_overlap'], = struct.unpack('>f', binary_data[34:38])
+    meas_hdr['meas_title'] = _strip_nonprintable(
+            struct.unpack('>60s', binary_data[38:98])[0])
+    meas_hdr['video_bw'], = struct.unpack('>f', binary_data[98:102])
+    (meas_hdr['center_freq'], meas_hdr['span_freq'],
+            meas_hdr['sweep_freq']) = \
+            struct.unpack('>3d', binary_data[102:126])
+    coded_meas_type, = struct.unpack('>h', binary_data[126:128])
+    # FIXME: Need to add the other measurement types
+    meas_type_decoder = {-99: 'Unknown measurement', 0: 'Spectrum measurement',
+            1: 'Network measurement', 2: 'Swept measurement',
+            3: 'FFT measurement'}
+    meas_hdr['meas_type'] = meas_type_decoder[coded_meas_type]
+    coded_real_time, = struct.unpack('>h', binary_data[128:130])
+    real_time_decoder = {0: 'Not continuous', 1: 'Continuous'}
+    meas_hdr['real_time'] = real_time_decoder[coded_real_time]
+
+    if sdf_revision == 1:
+        # Decode the revision 1 stuff
+        # FIXME: Add rev 1 stuff later
+        pass
+    elif sdf_revision == 2:
+        # Decode the revision 2 stuff
+        (meas_hdr['start_freq_index'], meas_hdr['stop_freq_index']) = \
+                struct.unpack('>2h', binary_data[24:28])
+    elif sdf_revision == 3:
+        # Decode the revision 3 related stuff
+        # FIXME: Add rev 3 stuff later
+        pass
+    else:
+        # SDF revision not recognized
+        sys.exit('Did not recognize SDF revision')
+
+
+
+    return meas_hdr
+
 
 def read_sdf_file(sdf_filename):
     '''
@@ -70,58 +169,34 @@ def read_sdf_file(sdf_filename):
             # Didn't find a valid file identifer, so bail out
             sys.exit('Did not find a valid file identifier.')
 
-        # Process the file header record
         # Determine record type (short) and record size (long)
+        # Every record has these two special fields at the start
         record_type_size_format = '>hl'
-        record_type, record_size = struct.unpack(record_type_size_format,
+
+        # Process the file header record
+        file_hdr_record_type, file_hdr_record_size = struct.unpack(
+                record_type_size_format,
                 sdf_file.read(struct.calcsize(record_type_size_format)))
-        if record_type == 10:
+        if file_hdr_record_type == 10:
             # Found the file header record
-            sdf_hdr['file_hdr'] = _process_sdf_file_hdr(record_type, record_size,
-                    sdf_file.read(record_size - struct.calcsize(record_type_size_format)))
+            # Process the entire file header record including the record type
+            # (short) and record size (long)
+            sdf_file.seek(sdf_file.tell() - struct.calcsize(record_type_size_format))
+            sdf_hdr['file_hdr'] = _decode_sdf_file_hdr(file_hdr_record_size,
+                    sdf_file.read(file_hdr_record_size))
         else:
             sys.exit('Error processing SDF file; expected file header')
 
         # Process the measurement header record
-        # Determine record type (short) and record size (long)
-        record_type_size_format = '>hl'
-        record_type, record_size = struct.unpack(record_type_size_format,
+        meas_hdr_record_type, meas_hdr_record_size = struct.unpack(record_type_size_format,
                 sdf_file.read(struct.calcsize(record_type_size_format)))
-        if record_type == 11:
+        if meas_hdr_record_type == 11:
             # Found the measurement header record
-            pass
+            sdf_file.seek(sdf_file.tell() - struct.calcsize(record_type_size_format))
+            sdf_hdr['meas_hdr'] = _decode_sdf_meas_hdr(meas_hdr_record_size,
+                    sdf_hdr['file_hdr']['sdf_revision'], sdf_file.read(meas_hdr_record_size))
         else:
-            sys.exit('Error processing SDF file; expect measurement header')
-
-        all_records_processed = False
-        while not all_records_processed:
-            # Determine record type (short) and record size (long)
-            record_type_size_format = '>hl'
-            record_type, record_size = struct.unpack(record_type_size_format,
-                    sdf_file.read(struct.calcsize(record_type_size_format)))
-            print('Processing record_type {rec_type}, which is {rec_size} bytes'.format(
-                rec_type=record_type,
-                rec_size=record_size))
-            if record_type == 12:
-                # Process a data header record (could be more than one)
-                sdf_hdr['data_hdr'].append(_process_sdf_data_hdr(record_type,
-                    record_size, sdf_file.read(record_size - struct.calcsize(
-                        record_type_size_format))))
-            elif record_type == 13:
-                # Process a vector header record (could be more than one)
-                sdf_hdr['vector_hdr'].append(_process_sdf_vector_hdr(record_type,
-                    record_size, sdf_file.read(record_size - struct.calcsize(
-                        record_type_size_format))))
-            elif record_type == 14:
-                # Process channel header record (could be more than one)
-                sdf_hdr['channel_hdr'].append('Process me.')
-            elif record_type == 15:
-                # Process a scan structure record
-                sdf_hdr['scan_structure'] = 'Process me.'
-
-            # Only read one record at this point
-            all_records_processed = True
-
+            sys.exit('Error processing SDF file; expected measurement header')
 
 
         #dt = np.dtype([('file_id', '|a2')])
--- tests/test_sdfascii.py
+++ tests/test_sdfascii.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import datetime
 import os
 import unittest
 
@@ -24,3 +25,120 @@ class TestReadingSDFFormat(unittest.TestCase):
     def test_reading_sdf_file_identifier(self):
         self.assertTrue(self.sdf_hdr['valid_file_identifier'],
                 'Invalid SDF file identifier')
+
+    def test_sdf_file_hdr_record_size(self):
+        self.assertEqual(self.sdf_hdr['file_hdr']['record_size'],64)
+
+    def test_sdf_file_hdr_sdf_revision(self):
+        self.assertEqual(self.sdf_hdr['file_hdr']['sdf_revision'],2)
+
+    def test_sdf_file_hdr_application(self):
+        self.assertEqual(self.sdf_hdr['file_hdr']['application'],'HP 35670A')
+
+    def test_sdf_file_hdr_measurement_start_datetime(self):
+        self.assertEqual(self.sdf_hdr['file_hdr']['measurement_start_datetime'],
+                datetime.datetime(2013, 2, 13, 9, 8))
+
+    def test_sdf_file_hdr_application_version(self):
+        self.assertEqual(self.sdf_hdr['file_hdr']['application_version'],
+                'A.01.11')
+
+    def test_sdf_file_hdr_num_data_hdr_records(self):
+        self.assertEqual(self.sdf_hdr['file_hdr']['num_data_hdr_records'],1)
+
+    def test_sdf_file_hdr_num_vector_hdr_records(self):
+        self.assertEqual(self.sdf_hdr['file_hdr']['num_vector_hdr_records'],1)
+
+    def test_sdf_file_hdr_num_channel_hdr_records(self):
+        self.assertEqual(self.sdf_hdr['file_hdr']['num_channel_hdr_records'],2)
+
+    def test_sdf_file_hdr_num_unique_records(self):
+        self.assertEqual(self.sdf_hdr['file_hdr']['num_unique_records'],1)
+
+    def test_sdf_file_hdr_num_scan_struct_records(self):
+        self.assertEqual(self.sdf_hdr['file_hdr']['num_scan_struct_records'],1)
+
+    def test_sdf_file_hdr_num_xdata_records(self):
+        self.assertEqual(self.sdf_hdr['file_hdr']['num_xdata_records'],0)
+
+    def test_sdf_file_hdr_offset_data_hdr_record(self):
+        self.assertTrue('offset_data_hdr_record' in self.sdf_hdr['file_hdr'])
+        self.assertEqual(self.sdf_hdr['file_hdr']['offset_data_hdr_record'],206)
+
+    def test_sdf_file_hdr_offset_vector_record(self):
+        self.assertTrue('offset_vector_record' in self.sdf_hdr['file_hdr'])
+        self.assertEqual(self.sdf_hdr['file_hdr']['offset_vector_record'],340)
+
+    def test_sdf_file_hdr_offset_channel_record(self):
+        self.assertTrue('offset_channel_record' in self.sdf_hdr['file_hdr'])
+        self.assertEqual(self.sdf_hdr['file_hdr']['offset_channel_record'],358)
+
+    def test_sdf_file_hdr_offset_unique_record(self):
+        self.assertTrue('offset_unique_record' in self.sdf_hdr['file_hdr'])
+        self.assertEqual(self.sdf_hdr['file_hdr']['offset_unique_record'],742)
+
+    def test_sdf_file_hdr_offset_scan_struct_record(self):
+        self.assertTrue('offset_scan_struct_record' in self.sdf_hdr['file_hdr'])
+        self.assertEqual(self.sdf_hdr['file_hdr']['offset_scan_struct_record'],1264)
+
+    def test_sdf_file_hdr_offset_xdata_record(self):
+        # Since a Python dictionary will return -1 if a key does not exist
+        # I had to add a test to make sure that the key does in fact exist
+        # I wouldn't have discovered this without writing the test first.
+        # TDD actually provided some value!
+        self.assertTrue('offset_xdata_record' in self.sdf_hdr['file_hdr'])
+        self.assertEqual(self.sdf_hdr['file_hdr']['offset_xdata_record'],-1)
+
+    def test_sdf_file_hdr_offset_ydata_record(self):
+        self.assertTrue('offset_ydata_record' in self.sdf_hdr['file_hdr'])
+        self.assertEqual(self.sdf_hdr['file_hdr']['offset_ydata_record'],1304)
+
+    def test_sdf_meas_hdr_record_size(self):
+        self.assertEqual(self.sdf_hdr['meas_hdr']['record_size'],140)
+
+    def test_sdf_meas_hdr_offset_unique_record(self):
+        self.assertTrue('offset_unique_record' in self.sdf_hdr['meas_hdr'])
+        self.assertEqual(self.sdf_hdr['meas_hdr']['offset_unique_record'],-1)
+
+    def test_sdf_meas_hdr_block_size(self):
+        self.assertEqual(self.sdf_hdr['meas_hdr']['block_size'],4096)
+
+    def test_sdf_meas_hdr_zoom_mode_on(self):
+        self.assertFalse(self.sdf_hdr['meas_hdr']['zoom_mode_on'])
+
+    def test_sdf_meas_hdr_start_freq_index(self):
+        self.assertEqual(self.sdf_hdr['meas_hdr']['start_freq_index'],0)
+
+    def test_sdf_meas_hdr_stop_freq_index(self):
+        self.assertEqual(self.sdf_hdr['meas_hdr']['stop_freq_index'],1600)
+
+    def test_sdf_meas_hdr_average_type(self):
+        self.assertEqual(self.sdf_hdr['meas_hdr']['average_type'],'None')
+
+    def test_sdf_meas_hdr_average_num(self):
+        self.assertEqual(self.sdf_hdr['meas_hdr']['average_num'],1)
+
+    def test_sdf_meas_hdr_pct_overlap(self):
+        self.assertEqual(self.sdf_hdr['meas_hdr']['pct_overlap'],0.0)
+
+    def test_sdf_meas_hdr_meas_title(self):
+        self.assertEqual(self.sdf_hdr['meas_hdr']['meas_title'],'')
+
+    def test_sdf_meas_hdr_video_bw(self):
+        self.assertEqual(self.sdf_hdr['meas_hdr']['video_bw'],0.0)
+
+    def test_sdf_meas_hdr_center_freq(self):
+        self.assertEqual(self.sdf_hdr['meas_hdr']['center_freq'],8192.0)
+
+    def test_sdf_meas_hdr_span_freq(self):
+        self.assertEqual(self.sdf_hdr['meas_hdr']['span_freq'],16384.0)
+
+    def test_sdf_meas_hdr_sweep_freq(self):
+        self.assertEqual(self.sdf_hdr['meas_hdr']['sweep_freq'],0.0)
+
+    def test_sdf_meas_hdr_meas_type(self):
+        self.assertEqual(self.sdf_hdr['meas_hdr']['meas_type'],'FFT measurement')
+
+    def test_sdf_meas_hdr_real_time(self):
+        self.assertEqual(self.sdf_hdr['meas_hdr']['real_time'],'Continuous')
+
